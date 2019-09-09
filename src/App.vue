5f1bdd485245b43d9c62d09b033588ff4ee87573
Ali Alizada <ali.alizada@qileap.com>
2019-09-09 10:11:53 -0700

    add todos

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <MyCustomComponent name="Ali Alizada" /> -->
+    <Todos v-bind:todos='todos' v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todos from './components/Todos.vue'
+// import MyCustomComponent from './components/MyNewComponent.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Todos,
+  },
+   data() {
+    return {
+     books: 1,
+     todos: [
+       {
+         id: 1,
+         title: 'go to Shopping',
+         completed: false
+       },
+        {
+         id: 2,
+         title: 'practice codes',
+         completed: true
+       },
+        {
+         id: 3,
+         title: 'send money',
+         completed: false
+       }
+     ]
+    }
+  },
+  methods: {
+  deleteTodo(id){
+      this.todos = this.todos.filter(todo=>  todo.id !== id)
+    }
+  },
+  beforeCreate() {
+    alert('beforCreate hook has been called');
+    console.log('beforCreate hook has been called');
+ },
+ created() {
+  alert('Created hook has been called');
+  console.log(`books is of type ${typeof this.books}`);
+ },
+ beforeMount() {
+    alert('beforeMount is called')
+  },
+    mounted(){
+    this.$data.hello= 'lalalalallalalalalaalal';
+ },
+  beforeUpdate(){
+    alert('beforeUpdate hook has been called');
+  },
+  updated(){
+    alert('Updated hook has been called');
+  },
+ beforeDestroy() {
+    this.books = null
+    delete this.books
+  },
+  destroyed() {
+    this.$destroy() 
+    console.log(this)
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
 </style>
---
+++ src/components/MyNewComponent.vue
@@ -0,0 +1,21 @@
+
+<template>
+    <div class="ali">
+        <h1>{{name}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+ name: 'MyCustomComponent',
+ props: {
+     name: String
+ }
+}
+</script>
+
+<style>
+h1 {
+    font-size: 50px;
+}
+</style>
